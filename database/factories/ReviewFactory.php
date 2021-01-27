adb266bdfc5c8f5ac55fc8ff6832c909233f37ed
TIGER <powertiger563@gmail.com>
2021-01-27 22:47:19 +0200

    Run Factories

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Review;
+use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5),
+        'product_id' => function(){
+            return Product::all()->random();
+        }
     ];
 });
